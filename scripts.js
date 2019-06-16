978dcc8476f41f918d7c536ce1a64d1aeb3bf7c0
diegotorres9 <diegoetorres1@gmail.com>
2019-06-16 12:42:22 -0400

    creating a variable for chevron class then adding to function to rotate chevrons

--- scripts.js
+++ scripts.js
@@ -19,16 +19,23 @@ function closemenu(event) {
   menu.classList.remove("nav-open");
   menuOpen.classList.remove("spin");
 }
+
 // Accordion Open and Closing
 var accordions = document.querySelectorAll(".accordion__header");
 
 accordions.forEach((accordion) => {
     accordion.addEventListener('click', () => {
-        var content =accordion.nextElementSibling;
+        // Grab the ul food list
+        var content = accordion.nextElementSibling;
+        // Granb the chevron inside of button and rotate on click
+        var chevron = accordion.querySelector(".chevron");
         if(content.style.maxHeight){
             content.style.maxHeight = null;
+            chevron.classList.add("rotate-chevron-down");
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
+            chevron.classList.add("rotate-chevron-up");
+            chevron.classList.remove("rotate-chevron-down");
         }
         event.preventDefault();
     });
